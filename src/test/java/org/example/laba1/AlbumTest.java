8054412a7d61b2553add138451641efe17c49cc2
MasterIlidan <segamil2001@gmail.com>
2024-08-13 18:50:18 +0500

    initial commit

---
+++ src/test/java/org/example/laba1/AlbumTest.java
@@ -0,0 +1,28 @@
+package org.example.laba1;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlbumTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        final int points_size = 1;
+        final String name = "HIStory: Past, Present and Future, Book I";
+
+    }
+}
